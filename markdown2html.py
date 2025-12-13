f086bc9e99a2646ade64b8d255396634de018f53
totomus83 <tom.dibellonio@gmail.com>
2025-12-13 18:29:04 +0100

    test

--- markdown2html.py
+++ markdown2html.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-'''
+"""
 Write a script that takes an argument 2 strings
-'''
+"""
 import sys
 import os
 
 
 def main():
-    '''
+    """
     Main entry point for the script
-    '''
-    if len(sys.argv) < 3:
+    """
+    if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
     md_file = sys.argv[1]
+    html_file = sys.argv[2]
 
     if not os.path.isfile(md_file):
         print(f"Missing {md_file}", file=sys.stderr)
